35e5e3ffdf39d694eeb684714d630c8ddcb324ab
Sahand Seifi <sahand.seifi@gmail.com>
2022-03-03 11:35:06 -0330

    First draft, lacks CICD

---
+++ src/NotificationAPI/NotificationAPI.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace NotificationAPI;
+
+class NotificationAPI
+{
+    public $clientId;
+    public $clientSecret;
+    function __construct($clientId, $clientSecret)
+    {
+        if (!$clientId) {
+            throw 'Bad clientId';
+        }
+
+        if (!$clientSecret) {
+            throw 'Bad clientSecret';
+        }
+
+        $this->clientId = $clientId;
+        $this->clientSecret = $clientSecret;
+    }
+
+    public function send($sendRequest)
+    {
+        return $this->request('POST', 'sender', $sendRequest);
+    }
+
+    public function retract($retractRequest)
+    {
+        return $this->request('POST', 'sender/retract', $retractRequest);
+    }
+
+    public function createSubNotification($params)
+    {
+        $data = new \stdClass();
+        $data->title = $params['title'];
+        return $this->request("PUT", 'notifications/' . $params['notificationId'] . '/' . 'subNotifications/' . $params['subNotificationId'], $data);
+    }
+
+    public function deleteSubNotification($params)
+    {
+        return $this->request("DELETE", 'notifications/' . $params['notificationId'] . '/' . 'subNotifications/' . $params['subNotificationId'], null);
+    }
+
+    public function setUserPreferences($userId, $userPreferences)
+    {
+        return $this->request('POST', 'user_preferences/' . $userId, $userPreferences);
+    }
+
+    public function request($method, $uri, $data)
+    {
+
+        $curl = curl_init();
+
+        $url = "https://api.notificationapi.com/" . $this->clientId . "/" . $uri;
+
+        if ($data) {
+            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+        }
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
+            'Authorization: Basic ' . base64_encode($this->clientId . ":" . $this->clientSecret)
+        ]);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $data = curl_exec($curl);
+        $info = curl_getinfo($curl);
+        curl_close($curl);
+
+        if ($info['http_code'] >= 300) {
+            print_r([
+                "NotificationAPI error.",
+                $data,
+                $info
+            ]);
+        }
+
+        if ($info['http_code'] == "202") {
+            print_r([
+                "NotificationAPI warning.",
+                $data,
+                $info
+            ]);
+        }
+
+        return $data;
+    }
+}
